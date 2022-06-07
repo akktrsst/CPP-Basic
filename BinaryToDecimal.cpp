f92a2e75345518aabc5c0349a0a86f1fa1a1ee61
Abhishek Kumar <106051995+akktrsst@users.noreply.github.com>
2022-06-08 00:36:22 +0530

    Operators and For Loop

--- BinaryToDecimal.cpp
+++ BinaryToDecimal.cpp
@@ -1,3 +1,14 @@
+// Given a binary number as an integer N, convert it into decimal and print.
+// Input format :
+// An integer N in the Binary Format
+// Output format :
+// Corresponding Decimal number (as integer)
+// Constraints :
+// 0 <= N <= 10^9
+// Sample Input 1 :
+// 1100
+// Sample Output 1 :
+// 12
 #include<iostream>
 using namespace std;
 
